970aa8137416acb1e45cb0443606f14dd41a125a
yurididomenico <yurididomenico93@gmail.com>
2022-10-25 14:17:43 +0200

    Snack 1: Check

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -12,7 +12,24 @@ Inserisci un numero, se è pari stampa il numero, se è dispari stampa il numero
 ( del numero inserito dall'utente: esempio se il numero dell'utente è 2, il numero successivo è 3  ).
 */
 
+//definisco una variabile
+let numero = prompt(`Numero: `);
+//converto in valore intero
+parseInt(numero);
 
+// condizione
+if(numero % 2 == 0)
+{
+    // stampa
+    console.log(numero);
+}
+else
+{
+    // incremento
+    numero++;
+    // stampa
+    console.log(numero);
+}
 
 
 
