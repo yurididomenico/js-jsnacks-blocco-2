29f1c4d053433bac6e068b92fdd09dcdd36aac88
yurididomenico <yurididomenico93@gmail.com>
2022-10-25 14:11:45 +0200

    Set Workspace

---
+++ assets/javascript/main.js
@@ -0,0 +1,34 @@
+// JAVASCRIPT //
+
+console.log("Ciao!");
+
+/*
+*Snack1*
+Inserisci un numero, se è pari stampa il numero, se è dispari stampa il numero successivo ( del numero inserito dall'utente: esempio se il numero dell'utente è 2, il numero successivo è 3  ).
+
+-Inserisci un numero
+-Se è pari stampa il numero
+-Se è dispari stampa il numero successivo
+( del numero inserito dall'utente: esempio se il numero dell'utente è 2, il numero successivo è 3  ).
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// ----- FINE :) -----
